2ca5eeadee24572eef890bca204879346c307a58
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-06-18 23:43:45 +0300

    commit changes

--- app/src/main/java/com/germiyanoglu/android/unigramproject/utils/FilePath.java
+++ app/src/main/java/com/germiyanoglu/android/unigramproject/utils/FilePath.java
@@ -6,8 +6,8 @@ import android.os.Environment;
 public class FilePath {
 
     // "storage/emulated/0"
-    //public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
-    public String ROOT_DIR  = "/sdcard";
+    public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
+    //public String ROOT_DIR  = "/sdcard";
 
     public String PICTURES = ROOT_DIR + "/Pictures";
     public String CAMERA = ROOT_DIR + "/DCIM/camera";
